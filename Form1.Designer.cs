eebd73d561121ab6d622f0244ee1ae43ba5c3c1a
SurFace81 <sur.f4ce@yandex.ru>
2023-10-28 22:01:25 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Stego
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn1 = new Button();
+            label2 = new Label();
+            label1 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(12, 12);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(174, 77);
+            btn1.TabIndex = 0;
+            btn1.Text = "Analize";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 103);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 20);
+            label2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.Location = new Point(192, 12);
+            label1.Name = "label1";
+            label1.ReadOnly = true;
+            label1.ScrollBars = RichTextBoxScrollBars.Vertical;
+            label1.Size = new Size(596, 420);
+            label1.TabIndex = 3;
+            label1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(btn1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn1;
+        private Label label2;
+        private RichTextBox label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Stego
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static byte[] ImageToByte(Bitmap img)       // bitmap to byte array
+        {
+            // Ex. (cat)
+            //return new byte[] {0b11111101, 0b00000010, 0b00000000,
+            //                  0b00000011, 0b00000001, 0b00000010,
+            //                  0b11111100, 0b11111101, 0b00000001,
+            //                  0b00000011, 0b00000001, 0b11111100};
+
+            byte[] result = new byte[img.Width * img.Height * 3];
+
+            int i = 0;
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    Color clr = img.GetPixel(x, y);
+
+                    result[i] = clr.R;
+                    result[++i] = clr.G;
+                    result[++i] = clr.B;
+
+                    i += 1;
+                }
+            }
+
+            return result;
+        }
+
+        private static byte[] Decode(byte[] temp)
+        {
+            // Decode text from sequence of bytes
+            byte[] result = new byte[temp.Length / 4];
+
+            int j = 0;
+            for (int i = 0; i < temp.Length; i += 4)
+            {
+                byte resultByte = 0;
+                if (i + 3 < temp.Length)
+                {
+                    // Ex.
+                    // 24, 75, 177, 24 => 52; 24 = 00011000b, LSB = 00
+                    // 00 << 6 = 00000000, 11 << 4 = 00110000, 01 << 2 = 00000100, 00 << 0 = 00000000
+                    // 00000000 | 00110000 | 00000100 | 00000000 = 00110100b = 52
+                    resultByte = (byte)((temp[i] << 6) | (temp[i + 1] << 4) | (temp[i + 2] << 2) | temp[i + 3]);
+                }
+
+                if (j < result.Length)
+                {
+                    result[j] = resultByte;
+                }
+                j += 1;
+            }
+
+
+            return result; // Char codes in ASCII
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap("D:\\TestFiles\\1027.png"); // replace with your own
+            label2.Text = bmp.PixelFormat.ToString();           // Pixel format ARGB or RGB. (ARGB not work)
+
+            byte[] arr = ImageToByte(bmp);
+            byte[] temp = new byte[arr.Length];
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                temp[i] = (byte)(arr[i] & 0x03);
+            }
+
+
+            // Print
+            label1.Text = Encoding.ASCII.GetString(Decode(temp)).Replace("\\n", ""); ;
+        }
+    }
+}
